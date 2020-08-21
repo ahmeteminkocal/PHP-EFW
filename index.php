4e607ad803c84aba02696a5509733e3b416c5f54
Ahmet Emin Koçal <ahmetemin08@gmail.com>
2020-08-21 10:04:19 -0700

    Minor changes

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 use efwEngine\database;
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-define("SYS_DEBUG"
-    , true);
+define("SYS_DEBUG", true);
 include __DIR__."/efwSystem/efwEngine/global/startup.php";
 \efwEngine\startup::init();
 
